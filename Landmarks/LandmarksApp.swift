2567577f63c6ca6105e8888863d96578974d84d7
Lucas Berger <lucasberger2399@gmail.com>
2024-07-05 17:02:14 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Lucas Berger on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
